095d6cdec6e582fe322db06c258dcf74d454a383
Alex Hrabovyi <alexgraboviypro@gmail.com>
2022-06-01 18:33:27 +0300

    Create "for children online" page

--- src/js-libs/educationTabs.js
+++ src/js-libs/educationTabs.js
@@ -1,23 +1,3 @@
-"use strict";
-
-// ----- Mobile card slider init script -----
-
-const education_mobile_slider = new Swiper(".education-packs-wrapper__mobile-content", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    initialSlide: 1,
-
-    breakpoints: {
-
-        577: {
-            spaceBetween: 80,
-        }
-    }
-})
-
-// ===========================
-
 class EducationTabs {
     constructor(options) {
         this.educationBlock = document.querySelector(options.education_block_selector);
@@ -199,32 +179,3 @@ class EducationTabs {
     }
 
 }
-
-new EducationTabs({
-    education_block_selector: ".education-packs-wrapper__desktop-block",
-
-    button_active_class: "education-packs-wrapper__button_active",
-    card_hide_class: "element-none",
-
-    animation_hide_class: "animation-fading-down",
-    animation_show_class: "animation-fading-up",
-
-    selected_main_id: 0,
-    selected_additional_id: 0,
-
-    additional_tabs: true,
-});
-
-new EducationTabs({
-    education_block_selector: ".education-packs-wrapper__mobile-block",
-
-    button_active_class: "education-packs-wrapper__button_active",
-    card_hide_class: "element-none",
-
-    animation_hide_class: "animation-fading-down",
-    animation_show_class: "animation-fading-up",
-
-    selected_main_id: 0,
-
-    additional_tabs: true,
-});
---
+++ src/modules/education-packs/js/_education-packs-offline.js
@@ -0,0 +1,41 @@
+"use strict";
+
+{
+    const education_mobile_slider = new Swiper(".education-packs-wrapper__mobile-content", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        initialSlide: 1,
+
+        breakpoints: {
+
+            577: {
+                spaceBetween: 80,
+            }
+        }
+    })
+
+    new EducationTabs({
+        education_block_selector: ".education-packs-wrapper__desktop-block",
+
+        button_active_class: "education-packs-wrapper__button_active",
+        card_hide_class: "element-none",
+
+        animation_hide_class: "animation-fading-down",
+        animation_show_class: "animation-fading-up",
+
+        selected_main_id: 0,
+    });
+
+    new EducationTabs({
+        education_block_selector: ".education-packs-wrapper__mobile-block",
+
+        button_active_class: "education-packs-wrapper__button_active",
+        card_hide_class: "element-none",
+
+        animation_hide_class: "animation-fading-down",
+        animation_show_class: "animation-fading-up",
+
+        selected_main_id: 0,
+    });
+}
