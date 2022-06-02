8c16ad71d6080d7a9a0fd1bc45cc98ef212af1f7
Alex Hrabovyi <alexgraboviypro@gmail.com>
2022-06-02 17:47:45 +0300

    Finish education packs for adult script
    Page "for-adult" has been finished.

--- src/modules/education-packs-for-adult/js/education-packs-for-adult-scripts.js
+++ src/modules/education-packs-for-adult/js/education-packs-for-adult-scripts.js
@@ -1,242 +1,274 @@
 "use strict";
 
-class EducationPacks {
-    constructor(options) {
-        this.educationSection = document.querySelector(options.education_section_selector);
-        this.allRoundButtons = this.educationSection.querySelectorAll("[data-ed-round-button]");
-        this.allCheckBoxButtons = this.educationSection.querySelectorAll("[data-ed-button]");
-        this.allMessages = this.educationSection.querySelectorAll("[data-ed-message]");
-        this.cardsBlock = this.educationSection.querySelector(".education-packs-for-adult__cards-block");
+{
+    const education_mobile_slider = new Swiper(".education-packs-for-adult__cards-block_mobile", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        initialSlide: 1,
 
-        this.roundButtonChosenClass = options.round_button_chosen_class;
-        this.roundButtonActiveClass = options.round_button_active_class;
-        this.checkBoxActiveClass = options.checkbox_button_active_class;
-        this.messageHideClass = options.message_hide_class;
+        breakpoints: {
 
-        this.cardHideClass = options.card_hide_class;
-        this.animationHideClass = options.animation_hide_class;
-        this.animationShowClass = options.animation_show_class;
+            577: {
+                spaceBetween: 80,
+            }
+        }
+    })
 
-        this.displayedCardsId = 0;
+    class EducationPacks {
+        constructor(options) {
+            this.educationSection = document.querySelector(options.education_section_selector);
+            this.allRoundButtons = this.educationSection.querySelectorAll("[data-ed-round-button]");
+            this.allCheckBoxButtons = this.educationSection.querySelectorAll("[data-ed-button]");
+            this.allMessages = this.educationSection.querySelectorAll("[data-ed-message]");
+            this.cardsBlock = this.educationSection.querySelector(".education-packs-for-adult__cards-block");
 
-        this.typeOfCards;
+            this.roundButtonChosenClass = options.round_button_chosen_class;
+            this.roundButtonActiveClass = options.round_button_active_class;
+            this.checkBoxActiveClass = options.checkbox_button_active_class;
+            this.messageHideClass = options.message_hide_class;
 
-        this.setupCardsBlockHeight();
+            this.cardHideClass = options.card_hide_class;
+            this.animationHideClass = options.animation_hide_class;
+            this.animationShowClass = options.animation_show_class;
 
-        this.educationSection.addEventListener("click", this.handler.bind(this));
-    }
+            this.displayedCardsId = 0;
 
-    setupCardsBlockHeight() {
-        let card = this.educationSection.querySelector("[data-ed-card]");
+            this.typeOfCards;
 
-        card.classList.remove(this.cardHideClass);
+            this.setupCardsBlockHeight();
 
-        this.cardsBlockHeight = getComputedStyle(card).height;
-
-        card.classList.add(this.cardHideClass);
-    }
-
-    handler(e) {
-        e.preventDefault();
-
-        let currentButton = e.target.closest("[data-ed-button]");
-
-        if (!currentButton) return;
-
-        let id = +currentButton.dataset.edButton;
-
-        if (id === 1 && currentButton.hasAttribute("data-ed-offline-button")) {
-            this.typeOfCards = currentButton.dataset.edOfflineButton;
-        } else if (id === 2 && currentButton.hasAttribute("data-ed-online-button")) {
-            this.typeOfCards = currentButton.dataset.edOnlineButton;
+            this.educationSection.addEventListener("click", this.handler.bind(this));
         }
 
-        this.setupRoundButtons(id);
-        this.setupCheckBoxButtons(id, currentButton);
-        this.setupMessages(id, currentButton);
+        setupCardsBlockHeight() {
+            let card = this.educationSection.querySelector("[data-ed-card]");
 
-        if (id === 2) {
-            this.cardsHandler();
-        } else {
-            this.cardsHideHandler();
+            this.cardsBlockHeight = getComputedStyle(card).height;
+
+            card.classList.add(this.cardHideClass);
+
+            console.log(this.cardsBlockHeight);
         }
-    }
 
-    cardsHandler() {
-        let id;
+        handler(e) {
+            e.preventDefault();
 
-        switch (this.typeOfCards) {
-            case "offline-ind":
-                id = 1;
+            let currentButton = e.target.closest("[data-ed-button]");
 
-                this.cardsShowHandler(id);
-                break;
+            if (!currentButton) return;
 
-            case "offline-group":
-                id = 2;
+            let id = +currentButton.dataset.edButton;
 
-                this.cardsShowHandler(id);
-                break;
+            if (id === 1 && currentButton.hasAttribute("data-ed-offline-button")) {
+                this.typeOfCards = currentButton.dataset.edOfflineButton;
+            } else if (id === 2 && currentButton.hasAttribute("data-ed-online-button")) {
+                this.typeOfCards = currentButton.dataset.edOnlineButton;
+            }
 
-            case "offline-talking":
-                id = 3;
+            this.setupRoundButtons(id);
+            this.setupCheckBoxButtons(id, currentButton);
+            this.setupMessages(id, currentButton);
 
-                this.cardsShowHandler(id);
-                break;
-
-            case "online-ind-30":
-                id = 4;
-
-                this.cardsShowHandler(id);
-                break;
-
-            case "online-ind-60":
-                id = 5;
-
-                this.cardsShowHandler(id);
-                break;
-
-            case "online-group":
-                id = 6;
-
-                this.cardsShowHandler(id);
-                break;
+            if (id === 2) {
+                this.cardsHandler();
+            } else {
+                this.cardsHideHandler();
+            }
         }
-    }
 
-    cardsShow(cards) {
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].classList.remove(this.cardHideClass);
-            cards[i].classList.add(this.animationShowClass);
+        cardsHandler() {
+            let id;
 
-            cards[i].addEventListener("animationend", () => {
-                cards[i].classList.remove(this.animationShowClass);
-                this.educationSection.style.pointerEvents = "";
-            }, { once: true });
+            switch (this.typeOfCards) {
+                case "offline-ind":
+                    id = 1;
+
+                    this.cardsShowHandler(id);
+                    break;
+
+                case "offline-group":
+                    id = 2;
+
+                    this.cardsShowHandler(id);
+                    break;
+
+                case "offline-talking":
+                    id = 3;
+
+                    this.cardsShowHandler(id);
+                    break;
+
+                case "online-ind-30":
+                    id = 4;
+
+                    this.cardsShowHandler(id);
+                    break;
+
+                case "online-ind-60":
+                    id = 5;
+
+                    this.cardsShowHandler(id);
+                    break;
+
+                case "online-group":
+                    id = 6;
+
+                    this.cardsShowHandler(id);
+                    break;
+            }
         }
-    }
 
-    cardsHide(cards) {
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].classList.add(this.animationHideClass);
+        cardsShow(cards) {
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].classList.remove(this.cardHideClass);
+                cards[i].classList.add(this.animationShowClass);
 
-            cards[i].addEventListener("animationend", () => {
-                cards[i].classList.remove(this.animationHideClass);
-                cards[i].classList.add(this.cardHideClass);
-
-            }, { once: true });
+                cards[i].addEventListener("animationend", () => {
+                    cards[i].classList.remove(this.animationShowClass);
+                    this.educationSection.style.pointerEvents = "";
+                }, { once: true });
+            }
         }
-    }
 
-    cardsShowHandler(id) {
-        this.educationSection.style.pointerEvents = "none";
+        cardsHide(cards) {
+            for (let i = 0; i < cards.length; i++) {
+                cards[i].classList.add(this.animationHideClass);
 
-        let currentCards = this.educationSection.querySelectorAll(`[data-ed-card="${id}"]`);
+                cards[i].addEventListener("animationend", () => {
+                    cards[i].classList.remove(this.animationHideClass);
+                    cards[i].classList.add(this.cardHideClass);
 
-        if (this.displayedCardsId != 0) {
-            let displayedCards = this.educationSection.querySelectorAll(`[data-ed-card="${this.displayedCardsId}"]`);
+                }, { once: true });
+            }
+        }
 
-            this.cardsHide(displayedCards);
+        cardsShowHandler(id) {
+            this.educationSection.style.pointerEvents = "none";
 
-            displayedCards[0].addEventListener("animationend", () => {
+            let currentCards = this.educationSection.querySelectorAll(`[data-ed-card="${id}"]`);
+
+            if (this.displayedCardsId != 0) {
+                let displayedCards = this.educationSection.querySelectorAll(`[data-ed-card="${this.displayedCardsId}"]`);
+
+                this.cardsHide(displayedCards);
+
+                displayedCards[0].addEventListener("animationend", () => {
+                    this.cardsBlock.style.height = this.cardsBlockHeight;
+
+                    this.cardsShow(currentCards);
+
+                }, { once: true })
+            } else {
+
                 this.cardsBlock.style.height = this.cardsBlockHeight;
 
                 this.cardsShow(currentCards);
 
-            }, { once: true })
-        } else {
-            this.cardsBlock.style.height = this.cardsBlockHeight;
-
-            this.cardsShow(currentCards);
-        }
-
-        this.displayedCardsId = id;
-    }
-
-    cardsHideHandler() {
-        if (this.displayedCardsId != 0) {
-            this.educationSection.style.pointerEvents = "none";
-
-            let displayedCards = this.educationSection.querySelectorAll(`[data-ed-card="${this.displayedCardsId}"]`);
-
-            this.cardsHide(displayedCards);
-            this.educationSection.style.pointerEvents = "";
-
-            this.cardsBlock.style.height = "0px";
-
-            this.displayedCardsId = 0;
-        }
-    }
-
-    setupRoundButtons(id) {
-        this.allRoundButtons[id].classList.add(this.roundButtonChosenClass)
-
-        if (id < 2) {
-            for (let i = id + 1; i < this.allRoundButtons.length; i++) {
-                this.allRoundButtons[i].classList.remove(this.roundButtonChosenClass)
-                this.allRoundButtons[i].classList.remove(this.roundButtonActiveClass)
+                education_mobile_slider.updateSlides()
             }
 
-            this.allRoundButtons[id + 1].classList.add(this.roundButtonActiveClass)
+            this.displayedCardsId = id;
         }
-    }
 
-    setupCheckBoxButtons(id, button) {
-        for (let i = 0; i < this.allCheckBoxButtons.length; i++) {
-            if (this.allCheckBoxButtons[i].dataset.edButton >= id) {
-                this.allCheckBoxButtons[i].classList.remove(this.checkBoxActiveClass);
+        cardsHideHandler() {
+            if (this.displayedCardsId != 0) {
+                this.educationSection.style.pointerEvents = "none";
+
+                let displayedCards = this.educationSection.querySelectorAll(`[data-ed-card="${this.displayedCardsId}"]`);
+
+                this.cardsHide(displayedCards);
+                this.educationSection.style.pointerEvents = "";
+
+                this.cardsBlock.style.height = "0px";
+
+                this.displayedCardsId = 0;
             }
         }
 
-        button.classList.add(this.checkBoxActiveClass);
-    }
+        setupRoundButtons(id) {
+            this.allRoundButtons[id].classList.add(this.roundButtonChosenClass)
 
-    setupMessages(id, button) {
-        for (let i = 0; i < this.allMessages.length; i++) {
-            if (this.allMessages[i].dataset.edMessage > id) {
-                this.allMessages[i].classList.add(this.messageHideClass);
+            if (id < 2) {
+                for (let i = id + 1; i < this.allRoundButtons.length; i++) {
+                    this.allRoundButtons[i].classList.remove(this.roundButtonChosenClass)
+                    this.allRoundButtons[i].classList.remove(this.roundButtonActiveClass)
+                }
+
+                this.allRoundButtons[id + 1].classList.add(this.roundButtonActiveClass)
             }
         }
 
-        let messagesNext = document.querySelectorAll(`[data-ed-message="${id + 1}"]`);
-
-        if (button.hasAttribute("data-ed-offline-button")) {
-            for (let i = 0; i < messagesNext.length; i++) {
-                if (messagesNext[i].hasAttribute("data-ed-offline-message")) {
-                    messagesNext[i].classList.remove(this.messageHideClass);
+        setupCheckBoxButtons(id, button) {
+            for (let i = 0; i < this.allCheckBoxButtons.length; i++) {
+                if (this.allCheckBoxButtons[i].dataset.edButton >= id) {
+                    this.allCheckBoxButtons[i].classList.remove(this.checkBoxActiveClass);
                 }
             }
-        } else if (button.hasAttribute("data-ed-ind-button")) {
-            for (let i = 0; i < messagesNext.length; i++) {
-                if (messagesNext[i].hasAttribute("data-ed-ind-message")) {
-                    messagesNext[i].classList.remove(this.messageHideClass);
+
+            button.classList.add(this.checkBoxActiveClass);
+        }
+
+        setupMessages(id, button) {
+            for (let i = 0; i < this.allMessages.length; i++) {
+                if (this.allMessages[i].dataset.edMessage > id) {
+                    this.allMessages[i].classList.add(this.messageHideClass);
                 }
             }
-        } else if (button.hasAttribute("data-ed-group-button")) {
-            for (let i = 0; i < messagesNext.length; i++) {
-                if (messagesNext[i].hasAttribute("data-ed-group-message")) {
-                    messagesNext[i].classList.remove(this.messageHideClass);
+
+            let messagesNext = this.educationSection.querySelectorAll(`[data-ed-message="${id + 1}"]`);
+
+            if (button.hasAttribute("data-ed-offline-button")) {
+                for (let i = 0; i < messagesNext.length; i++) {
+                    if (messagesNext[i].hasAttribute("data-ed-offline-message")) {
+                        messagesNext[i].classList.remove(this.messageHideClass);
+                    }
                 }
-            }
-        } else {
-            for (let i = 0; i < messagesNext.length; i++) {
-                if (messagesNext[i].hasAttribute("data-ed-online-message")) {
-                    messagesNext[i].classList.remove(this.messageHideClass);
+            } else if (button.hasAttribute("data-ed-ind-button")) {
+                for (let i = 0; i < messagesNext.length; i++) {
+                    if (messagesNext[i].hasAttribute("data-ed-ind-message")) {
+                        messagesNext[i].classList.remove(this.messageHideClass);
+                    }
+                }
+            } else if (button.hasAttribute("data-ed-group-button")) {
+                for (let i = 0; i < messagesNext.length; i++) {
+                    if (messagesNext[i].hasAttribute("data-ed-group-message")) {
+                        messagesNext[i].classList.remove(this.messageHideClass);
+                    }
+                }
+            } else {
+                for (let i = 0; i < messagesNext.length; i++) {
+                    if (messagesNext[i].hasAttribute("data-ed-online-message")) {
+                        messagesNext[i].classList.remove(this.messageHideClass);
+                    }
                 }
             }
         }
     }
+
+    new EducationPacks({
+        education_section_selector: ".education-packs-for-adult_desktop",
+
+        round_button_chosen_class: "education-packs-for-adult__round-button_chosen",
+        round_button_active_class: "education-packs-for-adult__round-button_active",
+        checkbox_button_active_class: "education-packs-for-adult__checkbox_active",
+        message_hide_class: "hidden",
+
+        card_hide_class: "element-none",
+        animation_hide_class: "animation-fading-down",
+        animation_show_class: "animation-fading-up",
+    })
+
+    new EducationPacks({
+        education_section_selector: ".education-packs-for-adult_mobile",
+
+        round_button_chosen_class: "education-packs-for-adult__round-button_chosen",
+        round_button_active_class: "education-packs-for-adult__round-button_active",
+        checkbox_button_active_class: "education-packs-for-adult__checkbox_active",
+        message_hide_class: "hidden",
+
+        card_hide_class: "element-none",
+        animation_hide_class: "animation-fading-down",
+        animation_show_class: "animation-fading-up",
+    })
 }
-
-new EducationPacks({
-    education_section_selector: ".education-packs-for-adult",
-
-    round_button_chosen_class: "education-packs-for-adult__round-button_chosen",
-    round_button_active_class: "education-packs-for-adult__round-button_active",
-    checkbox_button_active_class: "education-packs-for-adult__checkbox_active",
-    message_hide_class: "hidden",
-
-    card_hide_class: "element-none",
-    animation_hide_class: "animation-fading-down",
-    animation_show_class: "animation-fading-up",
-})
